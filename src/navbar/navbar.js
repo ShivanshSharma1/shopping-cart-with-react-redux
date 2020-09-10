fc6d9c2f7a9cd21926902a4c81e65d3b50fc69c2
ShivanshSharma1 <shivanshs331@gmail.com>
2020-09-10 16:51:37 +0530

    navbar-changes

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -9,7 +9,7 @@ const NavBar = ({Item, Cart, ItemData}) => {
     return (
         <Router>
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg navbar-light " style={{height:"75px", background: "#e3f2fd"}}>
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
