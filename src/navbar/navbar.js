358244c045c65093870c59f7ea0c74420afca7cc
ShivanshSharma1 <shivanshs331@gmail.com>
2020-09-10 17:07:33 +0530

    reversing route changes

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -16,13 +16,13 @@ const NavBar = ({Item, Cart, ItemData}) => {
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                <Link to="/shopping-cart-with-react-redux" class="nav-link" href="#">Home <span class="sr-only">(current)</span></Link>
+                <Link to="/" class="nav-link" href="#">Home <span class="sr-only">(current)</span></Link>
                 <Link to='/cart' class="nav-link" href="#">Cart</Link>
                 </div>
             </div>
             </nav>
             <Switch>
-                <Route exact path='/shopping-cart-with-react-redux'>
+                <Route exact path='/'>
                     <Item />
                 </Route>
                 <Route path='/cart'>
